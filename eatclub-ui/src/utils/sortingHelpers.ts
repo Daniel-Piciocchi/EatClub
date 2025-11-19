7518ffddf425656c3ee4bc972016c5c46a4a84eb
YOUR_USERNAME <daniel_piciocchi@outlook.com>
2025-11-19 14:19:34 +1100

    implemented sorting utils for best deal logic

---
+++ eatclub-ui/src/utils/sortingHelpers.ts
@@ -0,0 +1,39 @@
+import { Restaurant, Deal, RestaurantWithBestDeal } from '@/data/types'
+
+export const getBestDeal = (restaurant: Restaurant): Deal => {
+    const sortedDeals = [...restaurant.deals].sort((a, b) => {
+        if (a.lightning !== b.lightning) {
+            return a.lightning === 'true' ? -1 : 1
+        }
+
+        const discountDiff = Number(b.discount) - Number(a.discount)
+        if (discountDiff !== 0) return discountDiff
+
+        return Number(b.qtyLeft) - Number(a.qtyLeft)
+    })
+
+    return sortedDeals[0]
+}
+
+export const sortRestaurantsByBestDeal = (
+    restaurants: Restaurant[]
+): RestaurantWithBestDeal[] => {
+    const restaurantsWithBestDeal = restaurants.map((restaurant) => ({
+        ...restaurant,
+        bestDeal: getBestDeal(restaurant),
+    }))
+
+    return restaurantsWithBestDeal.sort((a, b) => {
+        const dealA = a.bestDeal
+        const dealB = b.bestDeal
+
+        if (dealA.lightning !== dealB.lightning) {
+            return dealA.lightning === 'true' ? -1 : 1
+        }
+
+        const discountDiff = Number(dealB.discount) - Number(dealA.discount)
+        if (discountDiff !== 0) return discountDiff
+
+        return Number(dealB.qtyLeft) - Number(dealA.qtyLeft)
+    })
+}
